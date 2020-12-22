291ea531c48fdbcde869f771735e09d5eb46d08a
Kacper Łochowski <klochowski2802@gmail.com>
2020-12-22 23:41:35 +0100

    chore: add config to deal with scss files and scss modules

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "development",
@@ -7,4 +8,47 @@ module.exports = merge(common, {
   devServer: {
     contentBase: "./dist",
   },
+  module: {
+    rules: [
+      {
+        test: /\.module.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /\.module.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 });
